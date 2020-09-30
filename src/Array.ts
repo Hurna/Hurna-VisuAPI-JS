78fc9769fe444052d52ead5dd222282e5a518cfd
Hybesis <michael.jeulinl@gmail.com>
2020-09-30 12:33:29 +0200

    ENH: Add Array visualization API

---
+++ src/Array.ts
@@ -0,0 +1,90 @@
+import { Visualizer } from './';
+
+export default class Array extends Visualizer {
+  /**
+   * Set an array to visualize.
+   *
+   * @param array
+   */
+  set(_array?: any[]) {
+    this.command('set', arguments);
+  }
+
+  /**
+   * Add/Move a ptr on a element of the array
+   *
+   * @param x the index to put the pointer on
+   */
+  setPtr(_key: string, _x?: number) {
+    this.command('setPtr', arguments);
+  }
+
+  /**
+   * Swap two elements of the array
+   *
+   * @param a first element
+   * @param b second element
+   */
+  swap(_a: string | number, _b: string | number) {
+    this.command('swap', arguments);
+  }
+
+  /**
+   * Notify that a value has been changed.
+   *
+   * @param x The index of the array.
+   * @param v The new value to change to.
+   */
+  patch(_x: number, _v?: any) {
+    this.command('patch', arguments);
+  }
+
+  /**
+   * Stop notifying that a value has been changed.
+   *
+   * @param x The index of the array.
+   */
+  depatch(_x: number) {
+    this.command('depatch', arguments);
+  }
+
+  /**
+   * Select a range of the array.
+   *
+   * @param bx The index to select inclusively from.
+   * @param ex The index to select inclusively to. If omitted, it will only select index `sx`.
+   */
+  select(_bx: number, _ex?: number) {
+    this.command('select', arguments);
+  }
+
+  /**
+   * Stop selecting a range of the array.
+   *
+   * @param bx The index to stop selecting inclusively from.
+   * @param ex The index to stop selecting inclusively to. If omitted, it will only stop selecting index `sx`.
+   */
+  deselect(_bx: number, _ex?: number) {
+    this.command('deselect', arguments);
+  }
+
+  /**
+   * Select a scope of the array.
+   *
+   * @param bx The index to select inclusively from.
+   * @param ex The index to select inclusively to. If omitted, it will only select index `sx`.
+   */
+  selectScope(_bx: number, _ex?: number) {
+    this.command('selectScope', arguments);
+  }
+
+  /**
+   * Stop selecting a range of the array.
+   *
+   * @param bx The index to stop selecting inclusively from.
+   * @param ex The index to stop selecting inclusively to. If omitted, it will only stop selecting index `sx`.
+   */
+  deselectScope(_bx: number, _ex?: number) {
+    this.command('deselectScope', arguments);
+  }
+}
